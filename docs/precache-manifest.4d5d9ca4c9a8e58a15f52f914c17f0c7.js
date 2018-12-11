e31546c446b0e95f67f96d84ad93c3e8126703fb
Matt Reimer <matt.reimer@gmail.com>
2018-12-11 12:21:25 -0800

    new build

--- docs/precache-manifest.4d5d9ca4c9a8e58a15f52f914c17f0c7.js
+++ docs/precache-manifest.4d5d9ca4c9a8e58a15f52f914c17f0c7.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/BDAPOC/static/media/outline-icons.cd6c777f.woff2"
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/BDAPOC/static/media/outline-icons.ad97afd3.ttf"
   },
   {
-    "revision": "0abd4d3fab7f2822b7c3",
+    "revision": "a3a6885e519c00d2e959",
     "url": "/BDAPOC/static/css/main.5e0d8f07.chunk.css"
   },
   {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "/BDAPOC/static/media/icons.962a1bf3.svg"
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/BDAPOC/static/media/outline-icons.ef60a4f6.woff"
   },
   {
     "revision": "23bf4839d348ae46122a",
@@ -20,48 +20,44 @@ self.__precacheManifest = [
     "url": "/BDAPOC/static/js/runtime~main.23e79784.js"
   },
   {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/BDAPOC/static/media/flags.9c74e172.png"
-  },
-  {
     "revision": "0ab54153eeeca0ce03978cc463b257f7",
     "url": "/BDAPOC/static/media/icons.0ab54153.woff2"
   },
   {
-    "revision": "faff92145777a3cbaf8e7367b4807987",
-    "url": "/BDAPOC/static/media/icons.faff9214.woff"
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/BDAPOC/static/media/flags.9c74e172.png"
   },
   {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "/BDAPOC/static/media/icons.8e3c7f55.eot"
   },
   {
-    "revision": "701ae6abd4719e9c2ada3535a497b341",
-    "url": "/BDAPOC/static/media/outline-icons.701ae6ab.eot"
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/BDAPOC/static/media/icons.faff9214.woff"
   },
   {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
     "url": "/BDAPOC/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "0abd4d3fab7f2822b7c3",
-    "url": "/BDAPOC/static/js/main.0abd4d3f.chunk.js"
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/BDAPOC/static/media/outline-icons.701ae6ab.eot"
   },
   {
-    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
-    "url": "/BDAPOC/static/media/outline-icons.ef60a4f6.woff"
+    "revision": "a3a6885e519c00d2e959",
+    "url": "/BDAPOC/static/js/main.a3a6885e.chunk.js"
   },
   {
-    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
-    "url": "/BDAPOC/static/media/outline-icons.ad97afd3.ttf"
-  },
-  {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/BDAPOC/static/media/brand-icons.13db00b7.eot"
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/BDAPOC/static/media/icons.962a1bf3.svg"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
     "url": "/BDAPOC/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/BDAPOC/static/media/brand-icons.13db00b7.eot"
   },
   {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
@@ -80,11 +76,15 @@ self.__precacheManifest = [
     "url": "/BDAPOC/static/media/brand-icons.a1a749e8.svg"
   },
   {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/BDAPOC/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
     "revision": "23bf4839d348ae46122a",
     "url": "/BDAPOC/static/css/1.1133e8a8.chunk.css"
   },
   {
-    "revision": "24ca3dfc1a4d86f9ed76c2804223ec58",
+    "revision": "07ea1357ff73b98e4e7fdf5779b937d4",
     "url": "/BDAPOC/index.html"
   }
 ];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/BDAPOC/precache-manifest.b7f0d6267f121a3b597779232016ddc3.js"
+  "/BDAPOC/precache-manifest.4d5d9ca4c9a8e58a15f52f914c17f0c7.js"
 );
 
 workbox.clientsClaim();
